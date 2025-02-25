409b73e77b33e4552100dc43a3b0325296341c00
Amanda Araujo <amanda_araujo@usp.br>
2025-02-24 22:55:02 -0300

    methods: public and private

--- classes.cpp
+++ classes.cpp
@@ -14,6 +14,21 @@ class Treco { // Class
         string nome;
 };
 
+class Coisa { // Class
+    public:
+        void coisaMetodo() {
+            cout << "Hello World! o/" << "\n";
+        }
+        void coisaMI6() {
+            cout << "Calling all the agents" << "\n";
+            coisaPrivado(); // Call inside the object itself
+        }
+    private:
+        void coisaPrivado() {
+            cout << "Secret agent";
+        }
+};
+
 int main() {
     Treco marreco; // Object of the class Treco
 
@@ -26,6 +41,18 @@ int main() {
     cout << "I'm a beautiful object!" << endl;
     std::cout << marreco.nome << "\n";
     std::cout << marreco.tamanho << "\n";
+
+    Treco recco;
+    recco.tamanho = 20;
+    recco.nome = "Ecco";
+    std::cout << recco.nome << "\n";
+    std::cout << recco.tamanho << "\n";
+
+    // Second class
+    Coisa coiso;
+    coiso.coisaMetodo();
+    //coiso.coisaPrivado(); // Error: private method
+    coiso.coisaMI6(); // Public using Private
 
     return 0;
 }
